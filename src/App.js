00baf32a931b70fca15a454b322eb23103bb3cb8
filipapico <69467046+filipapico@users.noreply.github.com>
2020-11-05 19:47:26 +0000

    API integration

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react"
 import Weather from "./Weather"
 
 import './App.css';
@@ -10,9 +11,9 @@ export default function App() {
         <Weather />
         <footer>
           <small>Coded by
-            <a href="https://www.linkedin.com/in/filipapico/" target="_blank" rel="noreferrer">Filipa Pico</a>, 
-            <a href="https://github.com/filipapico/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a> {" "}and
-            <a href="https://wonderful-edison-2f8028.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
+            {""} <a href="https://www.linkedin.com/in/filipapico/" target="_blank" rel="noreferrer">Filipa Pico</a>, 
+            {""} <a href="https://github.com/filipapico/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a> and
+            {""} <a href="https://wonderful-edison-2f8028.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
           </small>
         </footer>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,38 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios"
 import "./Weather.css"
 
 export default function Weather() {
-    return(
-     <div className="Weather">
-      <div className="row">
-        <div className="col-6">
-          <h1><strong>Lisbon</strong></h1>
-          <ul>
-            <li>Thursday, 12:50</li>
-            <li>5, November</li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <form className="form-inline">
-            <input className="form-control mr-2" type="search" placeholder="Enter city name" autoFocus="on"/>
-            <input className="btn btn-success" type="submit" value="Seach" />
-          </form>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy"/>
-          <span className="temperature">14</span>
-          <span className="unit">ºC | ºF</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li><strong>Clouds</strong></li>  
-            <li>Humidity: 67 %</li>  
-            <li>Wind speed: 6 km/h</li>  
-          </ul>          
-        </div>
-      </div>
+  let [city, setCity] = useState(null)
+  let [temperature, setTemperature] = useState(null)
+  let [description, setDescription] = useState(null)
+  let [humidity, setHumidity] = useState(null)
+  let [windSpeed, setWindSpeed] = useState(null)
+  
+  function updateCity(event) {
+    event.preventDefault()
+    setCity(event.target.value)
+  }
+
+  function displayWeather(response) {
+    console.log(response.data.main)
+    setTemperature(Math.round(response.data.main.temp))
+    setDescription(response.data.weather[0].description)
+    setHumidity(response.data.main.humidity)
+    setWindSpeed(Math.round(response.data.wind.speed))
+    }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    let apiKey = "f2741c2d8db0d12b06b1e9b5fcfef6a1"
+    let units = "metric"
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
+    axios.get(apiUrl).then(displayWeather)  
+  }
+
+  return(
+    <div className="Weather">
+     <div className="row">
+       <div className="col-6">
+         <h1 className="text-capitalize"><strong>{city}</strong></h1>
+         <ul>
+           <li>Thursday, 12:50</li>
+           <li>5, November</li>
+         </ul>
+       </div>
+       <div className="col-6">
+         <form onSubmit={handleSubmit} className="form-inline">
+           <input onChange={updateCity} className="form-control mr-2" type="search" placeholder="Enter city name" autoFocus="on"/>
+           <input className="btn btn-success" type="submit" value="Search" />
+         </form>
+       </div>
      </div>
+     <div className="row">
+       <div className="col-6">
+         <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy"/>
+         <span className="temperature">{temperature}</span>
+         <span className="unit">ºC | ºF</span>
+       </div>
+       <div className="col-6">
+         <ul>
+           <li><strong className="text-capitalize">{description}</strong></li>  
+           <li>Humidity: {humidity}%</li>  
+           <li>Wind speed: {windSpeed} km/h</li>  
+         </ul>          
+       </div>
+     </div>
+    </div>
     )
 }
