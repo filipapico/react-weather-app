936fdcd5f5bdc1587a707100941617ec80f6ae0c
filipapico <69467046+filipapico@users.noreply.github.com>
2020-11-05 11:50:18 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.css';
 
 export default function App() {
   return (
+    <div className="App">
     <h1>Weather App</h1>
+    <footer>This project was coded by <a href="https://www.linkedin.com/in/filipapico/" target="_blank">Filipa Pico</a>
+    {" "}is <a href="https://github.com/filipapico/react-weather-app" target="_blank">open-sourced on GitHub</a>
+    {" "}and <a href="https://wonderful-edison-2f8028.netlify.app/" target="_blank">hosted on Netlify</a>
+    </footer>
+    </div>
   );
 }
 
