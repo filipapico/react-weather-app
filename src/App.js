6167aa520929af3b5d92162a63c2975084b10587
filipapico <69467046+filipapico@users.noreply.github.com>
2020-11-05 18:20:42 +0000

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather />
-        <footer><small>Coded by <a href="https://www.linkedin.com/in/filipapico/" target="_blank">Filipa Pico</a>, <a href="https://github.com/filipapico/react-weather-app" target="_blank">open-sourced on GitHub</a>
-        {" "}and <a href="https://wonderful-edison-2f8028.netlify.app/" target="_blank">hosted on Netlify</a></small>
+        <footer>
+          <small>Coded by
+            <a href="https://www.linkedin.com/in/filipapico/" target="_blank" rel="noreferrer">Filipa Pico</a>, 
+            <a href="https://github.com/filipapico/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a> {" "}and
+            <a href="https://wonderful-edison-2f8028.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
+          </small>
         </footer>
       </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,32 +1,33 @@
 import React from "react";
 import "./Weather.css"
 
-export default function Weather(params) {
+export default function Weather() {
     return(
      <div className="Weather">
       <div className="row">
         <div className="col-6">
-          <h1>Lisbon</h1>
+          <h1><strong>Lisbon</strong></h1>
           <ul>
             <li>Thursday, 12:50</li>
             <li>5, November</li>
           </ul>
         </div>
         <div className="col-6">
-          <form>
-            <input className="form-control" type="search" placeholder="Enter city name" autoFocus="on"/>
+          <form className="form-inline">
+            <input className="form-control mr-2" type="search" placeholder="Enter city name" autoFocus="on"/>
             <input className="btn btn-success" type="submit" value="Seach" />
           </form>
         </div>
       </div>
       <div className="row">
         <div className="col-6">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
-          14ºC
+          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy"/>
+          <span className="temperature">14</span>
+          <span className="unit">ºC | ºF</span>
         </div>
         <div className="col-6">
           <ul>
-            <li>Clouds</li>  
+            <li><strong>Clouds</strong></li>  
             <li>Humidity: 67 %</li>  
             <li>Wind speed: 6 km/h</li>  
           </ul>          
